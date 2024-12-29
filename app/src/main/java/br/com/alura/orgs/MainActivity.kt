16d84cf189aeb241d105e0834530f5b92407f87f
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-12-29 15:35:25 -0300

    primeiros passos no Android : D

---
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.com.alura.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        view.setText("cesta de frutas")
+        setContentView(view)
+    }
+}
