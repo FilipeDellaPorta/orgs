56344cde0d3614104343bd09990f14335a8d6203
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-01-02 13:35:36 -0300

    criando o adapter do RecyclerView e refatorando código

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,18 +1,19 @@
-package br.com.alura.orgs
+package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
-import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import br.com.alura.orgs.R
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nome = findViewById<TextView>(R.id.nome)
-        nome.text = "Cesta de frutas"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter()
         val descricao = findViewById<TextView>(R.id.descricao)
         descricao.text = "Laranja, manga e maçã"
         val valor = findViewById<TextView>(R.id.valor)
