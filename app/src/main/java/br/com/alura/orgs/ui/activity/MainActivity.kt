373379beefbe3137f369693d4ae622c9cb80b187
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-01-02 18:55:03 -0300

    finalizado a implementação do adapter / RecycleView

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,10 +2,12 @@ package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import br.com.alura.orgs.R
+import java.math.BigDecimal
+import br.com.alura.orgs.model.Produto
 
 class MainActivity : Activity() {
 
@@ -13,10 +15,12 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter()
-        val descricao = findViewById<TextView>(R.id.descricao)
-        descricao.text = "Laranja, manga e maçã"
-        val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "19.99"
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(nome = "teste", descricao = "teste desc", valor = BigDecimal("29.99")),
+            Produto(nome = "teste2", descricao = "teste desc2", valor = BigDecimal("39.99")),
+            Produto(nome = "teste3", descricao = "teste desc3", valor = BigDecimal("19.99")),
+
+        ))
+        //recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
